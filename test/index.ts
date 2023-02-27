aebc4233b3d3dd245198a0ee444915304a796178
Diego Pérez <dperez2055@gmail.com>
2023-02-27 11:09:52 -0300

    adding dummy test

--- test/index.ts
+++ test/index.ts
@@ -3,10 +3,16 @@
 // "type": "module", package.json
 import test from 'node:test'
 import assert from 'assert/strict'
-import { describe, it } from 'node:test'
 
-describe('A thing', () => {
+import { describe, it,  } from 'node:test'
+
+describe('Dummy Tests', () => {
   test('synchronous passing test', (t: unknown) => {
-    assert.strictEqual(1, 1);
+    assert.strictEqual(1, 1)
+  })
+  test('Fetch asynchronous test', async() => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users/1')
+    const json = await response.json()
+    assert.equal(json.name, 'Leanne Graham')
   })
 })
